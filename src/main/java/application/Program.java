0c1a778f9017f13393e1cd2c01ab00a4ad206fee
JoseCarlos67 <josecarlos.silesteves@gmail.com>
2026-04-21 19:23:54 -0300

    refactor: criado classe CurrencyConversionService para fazer o trabalho de orquestrador

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -1,12 +1,9 @@
 package application;
 
-import models.ExchangeRate;
-import services.ApiCoins;
+import services.CurrencyConversionService;
 
 public class Program {
   public static void main(String[] args) {
-    ApiCoins apiCoins = new ApiCoins();
-    ExchangeRate value = apiCoins.requestConversion("USD", "BRL", 100.00);
-    System.out.println("Cotação atual " + value);
+    CurrencyConversionService.manager();
   }
 }
